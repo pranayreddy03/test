51936fcef4347c71ab44ea0745c1e0711ee71202
pranay <pranayreddy2011@gmail.com>
2017-07-16 19:41:55 -0500

    Name Calculator

---
+++ nameCalculator.js
@@ -0,0 +1,27 @@
+
+(function()
+{
+          angular.module('myApp',[])
+                 .controller('myController',function($scope)
+                 {
+                      $scope.name = "";
+                      $scope.totalValue = 0;
+                      $scope.displayNumeric = function()
+                      {
+                          var totalNameValue = calculateNumericString($scope.name);  // get the total value
+                          $scope.totalValue = totalNameValue;
+                       };
+
+                      function calculateNumericString(string)
+                      {
+                             var totalStringValue = 0;
+                             for(var i=0;i<string.length;i++)
+                             {
+                                   totalStringValue += string.charCodeAt(i);
+                             }
+                             return totalStringValue;
+                      }
+
+
+                 });
+})();
